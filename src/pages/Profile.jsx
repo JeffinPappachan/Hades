67591bfa1c01c7e1ab69873ef01aa24d8ba53a61
Jude <judeputhussery360@gmail.com>
2024-01-17 16:18:26 +0530

    changed page title

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 
 const Profile = () => {
   return (
-    
+    <Container size="4" width="100%">
+      <Flex align="center" justify="center" direction="column" gap="4">
         <h1>Profile</h1>
         <Button>
           <Link style={styles.link} to="/profile/syllabus">
