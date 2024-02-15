a2ed4e1d86de99f87685e9d6574c76520fcf7785
JEFFIN PAPPACHAN <jeffinpappachan110@gmail.com>
2024-02-15 17:37:20 +0530

    update1
    years update

--- src/pages/Syllabus.jsx
+++ src/pages/Syllabus.jsx
@@ -1,13 +1,22 @@
 import { Container, Flex } from "@radix-ui/themes";
 import SelectComponent from "../components/select";
-const option=["Malayalam","English","Mathematics","Chemistry","Computer"];
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
+const option = ["Malayalam", "English", "Mathematics", "Chemistry", "Computer"];
+
 
 const Syllabus = () => {
+  const { profile, setProfile } = useContext(DataContext);
   return (
     <Container size="4" width="100%">
       <Flex align="center" justify="center" direction="column" gap="4">
         <h1>Syllabus</h1>
-        <SelectComponent placeholder="Select Sylabus" title="sylabus" options={option} />
+        <strong>Years-{profile.years}</strong>
+        <SelectComponent
+          placeholder="Select Sylabus"
+          title="sylabus"
+          options={option}
+        />
       </Flex>
     </Container>
   );
